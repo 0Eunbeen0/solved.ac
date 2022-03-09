affd0240be0bb6c5ade2f9933d93cd75aa93211e
0Eunbeen0 <ros4312@naver.com>
2022-03-10 00:28:52 +0900

    not 4 numbers, max 10 numbers.

--- boj1475.py
+++ boj1475.py
@@ -1,17 +1,15 @@
-room_num=input()
-num_list=list(room_num)
-setNum=int(0)
-print(num_list)
-for  i in range(len(num_list)):
-    if num_list[i]=="9":
-        num_list[i]="6"
-
-if num_list.count("6")<=2:
-    for i in range(len(num_list)):
-        if setNum<=num_list.count(num_list[i]):
-            setNum=num_list.count(num_list[i])
-        else:
-            setNum=setNum
+rnum=input()
+num_list=list(rnum)
+count_list=[]
+for i in range(len(num_list)):
+    if num_list[i]=='9':
+        num_list[i]='6'
+six=num_list.count('6')
+if six%2==0:
+    count_list.append(six/2)
 else:
-    setNum=2
-print(setNum)
+    count_list.append(six//2+1)
+for i in range(len(num_list)):
+    if num_list[i]!='6':
+        count_list.append(num_list.count(num_list[i]))
+print(int(max(count_list)))
