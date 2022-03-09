d391a809017d02ca962b654028c59bbf661d842b
0Eunbeen0 <ros4312@naver.com>
2022-03-10 06:01:49 +0900

    success

--- boj1316.py
+++ boj1316.py
@@ -6,12 +6,12 @@ for t in range(n):
     is_group_rev=is_group[::-1]
     while(len(is_group)!=0):
         i=is_group[0]
-        remove_set={i}
+        removeSet={i}
         x=is_group.index(i)
         y=len(is_group)-is_group_rev.index(i)-1
         if is_group.count(i)==(y-x+1):
             tf=1
-            is_group=[k for k in is_group if k not in remove_set]
+            is_group=[k for k in is_group if k not in removeSet]
         else:
             tf=0
             break;
